15e240eb48c5ff40d9a1c41a001d7dd88eb9ed42
klltx <kostya.svetashov.01@gmail.com>
2022-03-21 19:51:12 +0400

    validation for inputs

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { NativeSyntheticEvent, StyleSheet, TextInput, TextInputChangeEventData } from 'react-native';
 import { Button, RadioButton } from 'react-native-paper';
 
@@ -6,24 +6,40 @@ import { Text, View } from '../components/Themed';
 
 export default function LoginScreen(props: any) {
   const [userGender, setUserGender] = useState<string>('male'),
-    [userName, setUserName] = useState<string>(),
-    [userAge, setUserAge] = useState<string>();
+    [userName, setUserName] = useState<string>(''),
+    [userAge, setUserAge] = useState<number>(0),
+    [isValidName, setIsValidName] = useState<boolean>(false),
+    [isValidAge, setIsValidAge] = useState<boolean>(false);
+
+  useEffect(() => {
+    if(userName.length > 2) setIsValidName(true)
+    else setIsValidName(false)
+    
+    if(userAge > 6 && userAge < 100) setIsValidAge(true)
+    else setIsValidAge(false)
+  }, [userAge, userName])
+
+  function isInvalid(): boolean {return !(isValidAge && isValidName)}
 
   return (
     <View style={styles.container}>
       <Text>Ввведите ваше имя:</Text>
       <TextInput style={ styles.input } placeholder="input name"
-        onChange={(text: NativeSyntheticEvent<TextInputChangeEventData>) => setUserName(text.nativeEvent.text)} />
+        onChange={(text: NativeSyntheticEvent<TextInputChangeEventData>) => 
+          setUserName(text.nativeEvent.text)} />
       <Text>Введите ваш возраст:</Text>
       <TextInput style={ styles.input } placeholder="input age"
         keyboardType="numeric"
-        onChange={(text: NativeSyntheticEvent<TextInputChangeEventData>) => setUserAge(text.nativeEvent.text)} />
+        onChange={(text: NativeSyntheticEvent<TextInputChangeEventData>) => 
+          setUserAge(+text.nativeEvent.text)} />
       <Text>Выберите пол:</Text>
       <Text>Мужской</Text>
       <RadioButton
         value="male"
         status={ userGender === 'male' ? 'checked' : 'unchecked' }
-        onPress={() => setUserGender('male')}
+        onPress={() => {
+          setUserGender('male')
+        }}
       />
       <Text>Женский</Text>
       <RadioButton
@@ -33,8 +49,9 @@ export default function LoginScreen(props: any) {
       />
       <Button
         onPress={() => {
-          props.navigation.navigate('Root', { userName, userAge, userGender })
+          props.navigation.navigate('Root', { userName, userAge, userGender })          
         }}
+        disabled={isInvalid()}
         style={{ backgroundColor: 'blue' }}>
         <Text style={{ fontSize: 20, color: '#fff' }}>Войти</Text>
       </Button>
