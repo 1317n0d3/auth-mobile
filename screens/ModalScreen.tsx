ef6c1314b0add55d2d44493bbc8742561ef720b2
klltx <kostya.svetashov.01@gmail.com>
2022-03-22 14:52:03 +0400

    styles to modalscreen

--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -1,4 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { Platform, StyleSheet, Image, ImageProps, TextInput, TouchableOpacity } from 'react-native';
 import { Button } from 'react-native-paper';
 
@@ -9,30 +10,33 @@ import { RootTabScreenProps } from '../types';
 export default function ModalScreen({navigation, route}: RootTabScreenProps<'Modal'>) {
   const { userName, userAge, userGender } = route.params;
 
-  return (
-    <View style={styles.container}>
-      {/* <Text style={styles.title}>Modal</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/ModalScreen.tsx" /> */}
+  const [isLightTheme, setIsLightTheme] = useState<boolean>(true),
+    [name, setName] = useState<string>(userName);
 
-      
-      <Text style={styles.title}>Здравствуйте, {userName}</Text>
-      <Image source={require('../assets/images/userImage.jpg')} style={{ width: 100, height: 100, borderRadius: 100 }} />
-      <Button
-        onPress={() => alert('message')}
-        style={{ backgroundColor: 'blue' }}>
-        <Text style={{ fontSize: 20, color: '#fff' }}>Сменить фон</Text>
-      </Button>
-      <Button
-        onPress={() => alert('message')}
-        style={{ backgroundColor: 'blue' }}>
-        <Text style={{ fontSize: 20, color: '#fff' }}>Сменить имя</Text>
-      </Button>
-      <Button
-        onPress={() => alert('message')}
-        style={{ backgroundColor: 'blue' }}>
-        <Text style={{ fontSize: 20, color: '#fff' }}>Перейти</Text>
-      </Button>
+  return (
+    <View style={ isLightTheme ? styles.container : styles.containerDark}>
+      <Image source={require('../assets/images/userImage.jpg')} 
+        style={{ width: 100, height: 100, borderRadius: 100, marginTop: 30 }} />
+
+      <View style={isLightTheme ? undefined : styles.darkBackground}>
+        <Text style={ isLightTheme ? styles.title : styles.titleDark}>
+          {name}
+        </Text>
+
+        <Button
+          onPress={() => alert('message')}
+          style={styles.button}>
+          <Text style={{ fontSize: 20, color: '#fff' }}>Сменить имя</Text>
+        </Button>
+
+        <Button
+          onPress={() => {
+            setIsLightTheme(!isLightTheme)
+          }}
+          style={styles.button}>
+          <Text style={{ fontSize: 20, color: '#fff' }}>Сменить фон</Text>
+        </Button>
+      </View>
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
@@ -43,12 +47,30 @@ export default function ModalScreen({navigation, route}: RootTabScreenProps<'Mod
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  containerDark: {
+    backgroundColor: '#111',
+    flex: 1,
+    justifyContent: 'space-around',
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  darkBackground: {
+    backgroundColor: '#111',
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    padding: 10,
+  },
+  titleDark: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    padding: 10,
+    color: 'white',
   },
   separator: {
     marginVertical: 30,
@@ -60,5 +82,9 @@ const styles = StyleSheet.create({
     margin: 12,
     borderWidth: 1,
     padding: 10,
+  },
+  button: {
+    marginTop: 10,
+    backgroundColor: '#194bff',
   },
 });
