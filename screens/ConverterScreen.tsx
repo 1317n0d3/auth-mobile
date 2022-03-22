aaf3b5912d9aa61c12d34f7e027d45cde07d3ef2
klltx <kostya.svetashov.01@gmail.com>
2022-03-23 01:24:58 +0400

    add inputs

--- screens/ConverterScreen.tsx
+++ screens/ConverterScreen.tsx
@@ -30,6 +30,7 @@ export default function ConverterScreen() {
       ft: 'фут',
       mile: 'миля',
       inch: 'дюйм',
+      input: 'Введите число',
     },
     en: {
       metre: 'metre',
@@ -38,14 +39,16 @@ export default function ConverterScreen() {
       ft: 'ft',
       mile: 'mile',
       inch: 'inch',
+      input: 'Input number',
     },
   }
 
   const [selectedLanguage, setSelectedLanguage] = useState<string>('ru'),
-    [metric, setMetric] = useState<string>(metrics.metre);
+    [metric, setMetric] = useState<string>(metrics.metre),
+    [value, setValue] = useState<string>('');
 
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Picker
         selectedValue={selectedLanguage}
         onValueChange={(itemValue, itemIndex) =>
@@ -55,61 +58,73 @@ export default function ConverterScreen() {
         <Picker.Item label="English" value="en" />
       </Picker>
 
-      <RadioButton.Item
-        label={lang[selectedLanguage].metre}
-        color='#194bff'
-        value="male"
-        status={ metric === metrics.metre ? 'checked' : 'unchecked' }
-        onPress={() => {
-          setMetric(metrics.metre)
-        }}
-      />
-      <RadioButton.Item
-        label={lang[selectedLanguage].kilometer}
-        color='#194bff'
-        value="male"
-        status={ metric === metrics.kilometer ? 'checked' : 'unchecked' }
-        onPress={() => {
-          setMetric(metrics.kilometer)
-        }}
-      />
-      <RadioButton.Item
-        label={lang[selectedLanguage].centimeter}
-        color='#194bff'
-        value="male"
-        status={ metric === metrics.centimeter ? 'checked' : 'unchecked' }
-        onPress={() => {
-          setMetric(metrics.centimeter)
-        }}
-      />
-      <RadioButton.Item
-        label={lang[selectedLanguage].ft}
-        color='#194bff'
-        value="male"
-        status={ metric === metrics.ft ? 'checked' : 'unchecked' }
-        onPress={() => {
-          setMetric(metrics.ft)
-        }}
-      />
-      <RadioButton.Item
-        label={lang[selectedLanguage].mile}
-        color='#194bff'
-        value="male"
-        status={ metric === metrics.mile ? 'checked' : 'unchecked' }
-        onPress={() => {
-          setMetric(metrics.mile)
-        }}
-      />
-      <RadioButton.Item
-        label={lang[selectedLanguage].inch}
-        color='#194bff'
-        value="male"
-        status={ metric === metrics.inch ? 'checked' : 'unchecked' }
-        onPress={() => {
-          setMetric(metrics.inch)
-        }}
-      />
-    </View>
+      <TextInput style={ styles.input } placeholder={lang[selectedLanguage].input}
+        value={value}
+        keyboardType="numeric"
+        onChange={(text: NativeSyntheticEvent<TextInputChangeEventData>) => 
+          setValue(text.nativeEvent.text)} />
+
+      <View style={styles.metricsContainer}>
+        <View style={styles.metricsColumn}>
+          <RadioButton.Item
+            label={lang[selectedLanguage].metre}
+            color='#194bff'
+            value={metrics.metre}
+            status={ metric === metrics.metre ? 'checked' : 'unchecked' }
+            onPress={() => {
+              setMetric(metrics.metre)
+            }}
+          />
+          <RadioButton.Item
+            label={lang[selectedLanguage].kilometer}
+            color='#194bff'
+            value={metrics.kilometer}
+            status={ metric === metrics.kilometer ? 'checked' : 'unchecked' }
+            onPress={() => {
+              setMetric(metrics.kilometer)
+            }}
+          />
+          <RadioButton.Item
+            label={lang[selectedLanguage].centimeter}
+            color='#194bff'
+            value={metrics.centimeter}
+            status={ metric === metrics.centimeter ? 'checked' : 'unchecked' }
+            onPress={() => {
+              setMetric(metrics.centimeter)
+            }}
+          />
+        </View>
+        <View style={styles.metricsColumn}>
+          <RadioButton.Item
+            label={lang[selectedLanguage].ft}
+            color='#194bff'
+            value={metrics.ft}
+            status={ metric === metrics.ft ? 'checked' : 'unchecked' }
+            onPress={() => {
+              setMetric(metrics.ft)
+            }}
+          />
+          <RadioButton.Item
+            label={lang[selectedLanguage].mile}
+            color='#194bff'
+            value={metrics.mile}
+            status={ metric === metrics.mile ? 'checked' : 'unchecked' }
+            onPress={() => {
+              setMetric(metrics.mile)
+            }}
+          />
+          <RadioButton.Item
+            label={lang[selectedLanguage].inch}
+            color='#194bff'
+            value={metrics.inch}
+            status={ metric === metrics.inch ? 'checked' : 'unchecked' }
+            onPress={() => {
+              setMetric(metrics.inch)
+            }}
+          />
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
@@ -117,6 +132,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
+  },
+  metricsContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  metricsColumn: {
+    width: '50%',
+    backgroundColor: '#f5f5f5',
   },
   title: {
     fontSize: 18,
