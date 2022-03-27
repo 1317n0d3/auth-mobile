d4e885cda4e0260eb37f0444cb9532511272bb71
klltx <kostya.svetashov.01@gmail.com>
2022-03-27 21:46:49 +0400

    add notes endpoint

--- server/app.js
+++ server/app.js
@@ -1,46 +1,25 @@
 const sqlite3 = require("sqlite3").verbose();
 const express = require("express");
-
 const app = express();
+const http = require("http");
+const server = http.createServer(app);
+
 const port = 3000 || process.env.PORT;
 
 app.get("/", (req, res) => {
   res.json({ message: "alive" });
 });
 
+app.get("/notes", (req, res) => res.send(JSON.stringify()));
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
-let db = new sqlite3.Database("./notes.db", (err) => {
-  if (err) {
-    console.log(err.message);
-  }
+// let db = new sqlite3.Database("./notes.db", (err) => {
+//   if (err) {
+//     console.log(err.message);
+//   }
 
-  console.log("connected to notes.db");
-});
-
-db.serialize(() => {
-  db.run(
-    `CREATE TABLE notes(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    note text NOT NULL,
-    tags text NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL);`
-  );
-
-  db.run(
-    `INSERT into notes(note, tags) values ('first note', 'test'), ('second note', 'test');`,
-    (err, row) => {
-      if (err) {
-        console.log(err.message);
-      }
-
-      console.log("Values has been added ", row);
-    }
-  );
-
-  db.run(`SELECT * FROM notes`, (err, row) => {
-    console.log(row.id + ": " + row.info);
-  });
-});
+//   console.log("connected to notes.db");
+// });
