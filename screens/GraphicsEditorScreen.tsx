b6c699c003f505446d8ecd608b784f9ef7be3a11
klltx <kostya.svetashov.01@gmail.com>
2022-03-31 20:01:02 +0400

    draw square

--- screens/GraphicsEditorScreen.tsx
+++ screens/GraphicsEditorScreen.tsx
@@ -5,8 +5,30 @@ import { Button, RadioButton } from 'react-native-paper';
 import { Text, View } from '../components/Themed';
 
 export default function GraphicsEditorScreen() {
+  const [locationStart, setLocationStart] = useState({ x: 0, y: 0 }),
+    [locationEnd, setLocationEnd] = useState({ x: 0, y: 0 }),
+    figures = ([]);
+
+  function createFigure() {
+    return (
+      <View style={{ backgroundColor: 'gray', width: locationEnd.x - locationStart.x, height: locationEnd.y - locationStart.y, marginLeft: locationStart.x, marginTop: locationStart.y }} ></View>
+    )
+  }
+
   return (
-    <View style={styles.container}></View>
+    <View style={styles.container} 
+    onTouchStart={(e) => {
+      console.log(e.nativeEvent.locationX, e.nativeEvent.locationY);
+      setLocationStart({ x: e.nativeEvent.locationX, y: e.nativeEvent.locationY });      
+    }}
+    onTouchEnd={(e) => {
+      setLocationEnd({ x: e.nativeEvent.locationX, y: e.nativeEvent.locationY });  
+      figures.push(createFigure())    
+    }}>
+
+    { figures }
+
+    </View>
   );
 }
 
